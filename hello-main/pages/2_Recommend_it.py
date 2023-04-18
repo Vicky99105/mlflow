d6d1ef5f35098ac0e3594394a52d315c6caa7a40
Vicky99105 <vp991058@gmail.com>
2023-04-18 21:00:29 +0530

    directory

--- hello-main/pages/2_Recommend_it.py
+++ hello-main/pages/2_Recommend_it.py
@@ -9,7 +9,7 @@ st.write("## :red[Build a Recommender System 🤖]")
 
 st.write("##### We can build a recommender system to find the most relevant support article based on query")
 
-st.image("../Screenshot 2023-04-11 at 11.04.56 PM.png")
+st.image("./hello-main/Screenshot 2023-04-11 at 11.04.56 PM.png")
 
 col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
 if col7.button('Next'):
